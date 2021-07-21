d19e6578a560b55269588e63a49132813d8443c5
Ryan Stam <stam_ryan.e@hotmail.com>
2021-07-21 05:35:50 -0500

    Added multiple webpages

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ app.set('view engine', 'pug');
 app.set('views', './webpages');
 
 app.get('/', function (req, res) {
+    res.redirect('/homepage');
+});
+app.get('/homepage', function (req, res) {
     res.render('homepage');
 });
+
+app.get('/about-me', function (req, res) {
+    res.render('aboutMe');
+});
+app.get('/projects', function (req, res) {
+    res.render('projects');
+});
+app.get('/contact', function (req, res) {
+    res.render('contact');
+});
+
 
 app.listen(3000)
